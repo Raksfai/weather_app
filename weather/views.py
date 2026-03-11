3799d77c21f7a79ad87edf2c3f0459e6805c70f9
Raksfai <oleksander.veresaka05@gmail.com>
2026-03-11 22:58:19 +0200

    feat: add style.css, add output for user request and error exceptions

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,26 @@
+import requests
 from django.shortcuts import render
-
+from core.settings import WEATHER_API_KEY
 
 def index(request):
-    return render(request, "weather/index.html")
+    data = request.GET.get("city")
+    if not data:
+        return render(request, "weather/index.html")
+    print("User input:", data)
+    req = f"https://api.openweathermap.org/data/2.5/weather?q={data}&appid={WEATHER_API_KEY}&units=metric"
+    response = requests.get(req)
+    weather_data = response.json()
+    if weather_data.get("cod") != 200:
+        print("Error: 404", weather_data.get("cod"))
+        return render(request, "weather/index.html", {"error":"City wasn't found"})
+    context = {
+        'country': weather_data['sys']['country'],
+        'city': weather_data['name'],
+        'temp': weather_data['main']['temp'],
+        'feels_like': weather_data['main']['feels_like'],
+        'humidity': weather_data['main']['humidity'],
+        'pressure': weather_data['main']['pressure'],
+        'description': weather_data['weather'][0]['description'],
+    }
+    print("\n(TEST)Main weather data:", context.get("main", {}))
+    return render(request, "weather/index.html", context)
